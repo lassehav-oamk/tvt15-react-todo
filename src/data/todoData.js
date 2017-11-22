3ca0fd33875de9030dfa62b0c86a589cd319a9f0
Lasse Haverinen <lassehav@oamk.fi>
2017-11-22 14:01:05 +0200

    Added axios data get

--- src/data/todoData.js
+++ src/data/todoData.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 let data = [{"isDone":true,"description":"Post items","type":"chore","dueDate":"2017-10-18T21:00:00.000Z","id":"5086441721823232"},{"isDone":false,"description":"Get milk","type":"chore","dueDate":"2017-10-30T00:00:00.000Z","id":"5629499534213120"},{"dueDate":"2017-10-17T21:00:00.000Z","isDone":false,"description":"test","type":"t","id":"5639445604728832"},{"dueDate":"2017-10-27T21:00:00.000Z","isDone":false,"description":"testing","type":"s","id":"5644406560391168"},{"isDone":false,"description":"Buy winter tyres","type":"car","dueDate":"2017-10-31T22:00:00.000Z","id":"5649391675244544"},{"dueDate":"2017-10-26T21:00:00.000Z","isDone":false,"description":"Graphing","type":"test","id":"5668600916475904"},{"type":"learn","dueDate":"2017-11-06T13:18:02.000Z","isDone":false,"description":"Learn React","id":"5707702298738688"},{"dueDate":"2017-10-30T22:00:00.000Z","isDone":true,"description":"refactoring","type":"xx","id":"5732568548769792"}];
 
@@ -6,9 +7,15 @@ function getItems()
     // get the data from somewhere?
     return new Promise((resolve, reject) => {
 
-        resolve(data);
-
-        //reject("something failed");
+        axios.get("https://todoserver-182713.appspot.com/todos")
+        .then(results => {
+            console.log(results);
+            resolve(results.data);
+        })
+        .catch(error => {
+            console.log(error);
+            reject();
+        })
     });
 }
 
